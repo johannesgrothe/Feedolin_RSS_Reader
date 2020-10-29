598ff993e9bf998a739d200d76fe1bd959e661c4
Emircan Duman <s75316@beuth-hochschule.de>
2020-10-29 17:46:11 +0100

    #2 Added side menu

--- RSS_Reader/ContentView.swift
+++ RSS_Reader/ContentView.swift
@@ -9,72 +9,70 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
+    
+    @State var showMenu = false
+    
+    var body: some View {
+        
+        let drag = DragGesture()
+                    .onEnded {
+                        if $0.translation.width < -100 {
+                            withAnimation {
+                                self.showMenu = false
+                            }
+                        }
+                    }
+        
+        return NavigationView {
+            GeometryReader { geometry in
+            ZStack(alignment: .leading){
+                MainView(showMenu: self.$showMenu).frame(width: geometry.size.width, height: geometry.size.height)
+                    .offset(x: self.showMenu ? geometry.size.width/2 : 0)
+                    .disabled(self.showMenu ? true : false)
+                if self.showMenu {
+                    MenuView()
+                        .frame(width: geometry.size.width/2)
+                        .transition(.move(edge: .leading))
+                    }
+            }
+            .gesture(drag)
+        }
+            .navigationBarTitle("Side Menu", displayMode: .inline)
+            .navigationBarItems(leading: (
+                                Button(action: {
+                                    withAnimation {
+                                        self.showMenu.toggle()
+                                    }
+                                }) {
+                                    Image(systemName: "line.horizontal.3")
+                                        .imageScale(.large)
+                                }
+                            ))
+        }
+    }
+}
 
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+struct MainView: View {
+    
+    @Binding var showMenu: Bool
+    
     var body: some View {
         List {
-            ForEach(items) { item in
-                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+            Text("Article 1").onTapGesture {
+                print("Article 1")
             }
-            .onDelete(perform: deleteItems)
-        }
-        .toolbar {
-            #if os(iOS)
-            EditButton()
-            #endif
-
-            Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
+            Text("Article 2").onTapGesture {
+                print("Article 2")
             }
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            Text("Article 3").onTapGesture {
+                print("Article 3")
             }
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    static var previews:some View{
+        ContentView()
     }
 }
---
+++ RSS_Reader/MenuView.swift
@@ -0,0 +1,55 @@
+//
+//  MenuView.swift
+//  RSS_Reader
+//
+//  Created by Emircan Duman on 29.10.20.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Image(systemName: "person")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Profile")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 100)
+            HStack {
+                Image(systemName: "envelope")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Messages")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 30)
+            HStack {
+                Image(systemName: "gear")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Settings")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 30)
+            Spacer()
+        }
+        .padding()
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .background(Color(red: 32/255, green: 32/255, blue: 32/255))
+        .edgesIgnoringSafeArea(.all)
+        
+    }
+    
+    struct MenuView_Previews: PreviewProvider {
+        static var previews: some View {
+            MenuView()
+        }
+    }
+}
