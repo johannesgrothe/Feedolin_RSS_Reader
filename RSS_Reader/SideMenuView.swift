4dbe2bfc9bf42a03723140d19b7822e1e535b8b7
Katharina J. Kühn <kjohannakuehn@googlemail.com>
2020-11-14 10:33:16 +0100

    #4 reformat top buttons

--- RSS_Reader/SideMenuView.swift
+++ RSS_Reader/SideMenuView.swift
@@ -11,6 +11,7 @@ import SwiftUI
  Content of the side Menu
  */
 struct MenuContent: View {
+    @ObservedObject var model: Model = .shared
     
     func createMenuItem() -> [MenuItem] {
         var menu_item_list: [MenuItem] = []
@@ -30,32 +31,20 @@ struct MenuContent: View {
     
     var body: some View {
         VStack {
-            HStack {
-                
-                Image()
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 50, height: 50)
-         
-                Text(item.name)
-                    .font(.system(.title3, design: .rounded))
-                    .bold()
-            }
-            
-            Button(action: {
-                print("Show All Feeds Button pressed")
-            }) {
-                Label("All", systemImage: "newspaper")
-            }
-            
-            Button(action: {
-                print("Saved Button pressed")
-            }) {
-                Label("Saved", systemImage: "tray.2")
-            }
-            
-            
-    //        List(createMenuItem(), children: \.subMenuItems) { item in
+
+//            Button(action: {
+//                print("Show All Feeds Button pressed")
+//            }) {
+//                Label("All", systemImage: "newspaper")
+//            }
+//
+//            Button(action: {
+//                print("Saved Button pressed")
+//            }) {
+//                Label("Saved", systemImage: "tray.2")
+//            }
+                            
+//            List(createMenuItem(), children: \.subMenuItems) { item in
             List(sampleMenuItems, children: \.subMenuItems) { item in
     //        List {
                 HStack {
@@ -82,34 +71,17 @@ struct MenuItem: Identifiable {
     var subMenuItems: [MenuItem]?
 }
 
-// Sub-menu items for Espressco Machines
-let espressoMachineMenuItems = [ MenuItem(name: "Leva", image: "leva-x", subMenuItems: [ MenuItem(name: "Leva X", image: "leva-x"), MenuItem(name: "Leva S", image: "leva-s") ]),
-                                 MenuItem(name: "Strada", image: "strada-ep", subMenuItems: [ MenuItem(name: "Strada EP", image: "strada-ep"), MenuItem(name: "Strada AV", image: "strada-av"), MenuItem(name: "Strada MP", image: "strada-mp"), MenuItem(name: "Strada EE", image: "strada-ee") ]),
-                                 MenuItem(name: "KB90", image: "kb90"),
-                                 MenuItem(name: "Linea", image: "linea-pb-x", subMenuItems: [ MenuItem(name: "Linea PB X", image: "linea-pb-x"), MenuItem(name: "Linea PB", image: "linea-pb"), MenuItem(name: "Linea Classic", image: "linea-classic") ]),
-                                 MenuItem(name: "GB5", image: "gb5"),
-                                 MenuItem(name: "Home", image: "gs3", subMenuItems: [ MenuItem(name: "GS3", image: "gs3"), MenuItem(name: "Linea Mini", image: "linea-mini") ])
-                                ]
-
-// Sub-menu items for Grinder
-let grinderMenuItems = [ MenuItem(name: "Swift", image: "swift"),
-                         MenuItem(name: "Vulcano", image: "vulcano"),
-                         MenuItem(name: "Swift Mini", image: "swift-mini"),
-                         MenuItem(name: "Lux D", image: "lux-d")
-                        ]
-
-// Sub-menu items for other equipment
-let otherMenuItems = [ MenuItem(name: "Espresso AV", image: "espresso-av"),
-                         MenuItem(name: "Espresso EP", image: "espresso-ep"),
-                         MenuItem(name: "Pour Over", image: "pourover"),
-                         MenuItem(name: "Steam", image: "steam")
-                        ]
+// Main menu items
+let sampleMenuItems = [ MenuItem(name: "All", image: "newspaper", subMenuItems: nil),
+                        MenuItem(name: "Grinders", image: "swift-mini", subMenuItems: nil),
+                        MenuItem(name: "Other Equipment", image: "espresso-ep", subMenuItems: nil)
+                    ]
 
 // Main menu items
-let sampleMenuItems = [ MenuItem(name: "Espresso Machines", image: "linea-mini", subMenuItems: espressoMachineMenuItems),
-                        MenuItem(name: "Grinders", image: "swift-mini", subMenuItems: grinderMenuItems),
-                        MenuItem(name: "Other Equipment", image: "espresso-ep", subMenuItems: otherMenuItems)
-                    ]
+//let sampleMenuItems = [ MenuItem(name: "Espresso Machines", image: "linea-mini", subMenuItems: nil),
+//                        MenuItem(name: "Grinders", image: "swift-mini", subMenuItems: nil),
+//                        MenuItem(name: "Other Equipment", image: "espresso-ep", subMenuItems: nil)
+//                    ]
 
 /**
  View for the side menu
