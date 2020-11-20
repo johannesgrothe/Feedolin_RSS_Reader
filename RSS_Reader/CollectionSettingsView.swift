439437209897a2c6211dcf711db12b0aa4bae5fa
Katharina J. Kühn <kjohannakuehn@googlemail.com>
2020-11-20 16:03:00 +0100

    #14 Implemented View for CollectionsSettings

--- RSS_Reader/CollectionSettingsView.swift
+++ RSS_Reader/CollectionSettingsView.swift
@@ -9,9 +9,28 @@ import SwiftUI
 
 struct CollectionSettingsView: View {
     var body: some View {
-        Text("Hello, Collection Seetings View!")
         CollectionSettingsList()
             .navigationTitle("Collection Settings")
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Menu {
+                        Button(action: {
+                            print("Add New Collection Button clicked.")
+                        }) {
+                            Label("Add Collection", systemImage: "plus")
+                        }
+                        
+                        Button(action: {
+                            print("Remove Collection Button clicked.")
+                        }) {
+                            Label("Remove Collection", systemImage: "trash")
+                        }
+                    }
+                    label: {
+                        Label("Edit", systemImage: "square.and.pencil").imageScale(.large)
+                    }
+                }
+            }
     }
 }
 
@@ -24,7 +43,11 @@ struct CollectionSettingsList: View {
     
     var body: some View {
         List {
-            Text("Deine Muddaaa!")
+            ForEach(model.collection_data) { collection in
+                NavigationLink(destination: CollectionDetailSettingsView(collection: collection)) {
+                    Text(collection.name).font(.headline)
+                }
+            }
         .listRowBackground(Color.clear)
         }
         .onAppear(perform: {
@@ -37,11 +60,44 @@ struct CollectionSettingsList: View {
 }
 
 
-
+struct CollectionDetailSettingsView: View {
+    
+    // ToDo: comment
+    @ObservedObject var collection: Collection
+    
+    var body: some View {
+        List {
+            ForEach(collection.feed_list) { feed in
+                Text(feed.name).font(.headline)
+            }
+            .listRowBackground(Color.clear)
+            
+            Button(action: {
+                print("Add New Collection Button clicked.")
+            }) {
+                Label("Add Collection", systemImage: "plus")
+                    .frame(maxWidth: .infinity, alignment: .center)
+            }
+            .listRowBackground(Color.clear)
+        }
+        .onAppear(perform: {
+            UITableView.appearance().backgroundColor = .clear
+            UITableViewCell.appearance().backgroundColor = .clear
+        })
+        .background(Color(UIColor(named: "BackgroundColor")!))
+        .edgesIgnoringSafeArea(.bottom)
+    }
+}
 
 
 
 struct CollectionSettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        CollectionSettingsView()
+    }
+}
+
+struct CollectionDetailSettingsView_Previews: PreviewProvider {
     static var previews: some View {
         CollectionSettingsView()
     }
