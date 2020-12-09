11aa291e02c05e11063e4f57aa030cdfcb3e1a9d
Johannes Grothe <johannesgrothe@web.de>
2020-12-09 21:01:26 +0100

    #82 Renamed old regex method

--- RSS_Reader/helper_classes/FeedParser.swift
+++ RSS_Reader/helper_classes/FeedParser.swift
@@ -42,7 +42,7 @@ class FeedParser {
      Gets all regex groups from the selected regex and the passed text.
      DO NOT USE ANYMORE IT'S SHIT
      */
-    private func getRegexGroups_old(for regex: String, in text: String) -> [[String]] {
+    private func getRegexGroupsOldDontUse(for regex: String, in text: String) -> [[String]] {
         do {
             let regex = try NSRegularExpression(pattern: regex, options: .dotMatchesLineSeparators)
             let matches = regex.matches(in: text,
@@ -266,7 +266,7 @@ class FeedParser {
         complete_url = url.lowercased()
         
         // Check URL
-        let result_group = getRegexGroups_old(for: "(https?://)([a-z0-9]+)\\.([^:^/]*)(:\\d*)?(.*)?", in: complete_url!)
+        let result_group = getRegexGroupsOldDontUse(for: "(https?://)([a-z0-9]+)\\.([^:^/]*)(:\\d*)?(.*)?", in: complete_url!)
         if !result_group.isEmpty {
             let parsed_url_goup = result_group[0]
             self.main_url = parsed_url_goup[3]
