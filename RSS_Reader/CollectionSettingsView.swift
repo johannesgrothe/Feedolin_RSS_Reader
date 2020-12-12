8979e76b1e6c185c197ed9d76cfdf60d9a6ed743
Katharina J. Kühn <kjohannakuehn@googlemail.com>
2020-12-12 17:35:58 +0100

    #42 Rework of adding feed to collection

--- RSS_Reader/CollectionSettingsView.swift
+++ RSS_Reader/CollectionSettingsView.swift
@@ -91,68 +91,163 @@ struct CollectionDetailSettingsView: View {
     //line 92 is part of the workaround for back button bug
     @Environment(\.presentationMode) var presentationMode
     
+    /**
+     * ToDo: Add comment
+     */
+    @State private var editMode = EditMode.inactive
+
+    
+    @State private var editM = false
+    
+    @State private var presented_feed_list: [NewsFeed] = []
+    
+    
+    /**
+     * ToDo: Add comments
+     */
+    func fillFeedLis() {
+        print("call fill feed list")
+        if (editM) {
+            self.presented_feed_list = []
+            for feed_prov in model.feed_data {
+                for feed in feed_prov.feeds {
+                    self.presented_feed_list.append(feed)
+                }
+            }
+        } else {
+            self.presented_feed_list = collection.feed_list
+        }
+    }
+    
     var body: some View {
+        
         List {
-            ForEach(collection.feed_list) { feed in
+            Toggle("Hide", isOn: $editM)
+            
+            
+            ForEach(presented_feed_list) { feed in
                 // a row that contains the name of a feed, his parent token and icon
                 HStack {
+                    
                     feed.parent_feed!.icon.img
                         .resizable()
                         .frame(width: 25, height: 25)
                     Text("\(feed.parent_feed!.token) - \(feed.name)")//.font(.headline)
+                    Spacer()
+                    
+//                    values.filter { $0.isMultiple(of: 2) }
+                    if (editM) {
+                        
+                        if (checkBla(feed: feed)) {
+                           
+                            Button(action: {
+                                print("Delete Feed from Collection Button clicked.")
+                            }) {
+                                Image(systemName: "minus.circle.fill")
+                                    .foregroundColor(.red)
+                            }
+                        } else {
+                            Button(action: {
+                                print("Add Feed to Collection Button clicked.")
+                                let result = collection.addFeed(new_feed: feed)
+                                print(result)
+                            }) {
+                                Image(systemName: "plus.circle.fill")
+                                    .foregroundColor(.green)
+                            }
+                        }
+                            
+                            
+                    }
+                    
                 }
             }
-            .onDelete(perform: delete)
+            
+            .onChange(of: editM) { newValue in
+                fillFeedLis()
+            }
+
+            
+            
             .listRowBackground(Color.clear)
             
             Button(action:{ self.presentationMode.wrappedValue.dismiss() }){
-                                Text("Go Back")
+                                Text("!!!Go Back")
                             }
+        
         }
-        //
+        
+            
+        // enable edit
         .navigationBarItems(trailing: EditButton())
         //
+        
         .onAppear(perform: {
             UITableView.appearance().backgroundColor = .clear
             UITableViewCell.appearance().backgroundColor = .clear
+            fillFeedLis()
         })
         .background(Color(UIColor(named: "BackgroundColor")!))
         .edgesIgnoringSafeArea(.bottom)
         .navigationTitle(collection.name)
         
-        /*
-         .toolbar {
-            ToolbarItem(placement: .primaryAction) {
-                Menu {
-                    Button(action: {
-                        print("Add Feeds to Collection Button clicked.")
-                        self.show_add_feed_view.toggle()
-                    }) {
-                        Label("Add Feeds", systemImage: "plus")
-                    }
-                    
-                    Button(action: {
-                        print("Remove Feed from Collection Button clicked.")
-                    }) {
-                        Label("Remove Feed", systemImage: "trash")
-                    }
-                }
-                label: {
-                    Label("Edit", systemImage: "square.and.pencil").imageScale(.large)
-                }
-            }
-        }.sheet(isPresented: self.$show_add_feed_view) {
-            AddFeedsToCollectionView(collection: collection)
-        }
-        */
+       // /*
+//         .toolbar {
+//            ToolbarItem(placement: .primaryAction) {
+//                Menu {
+//                    Button(action: {
+//                        print("Add Feeds to Collection Button clicked.")
+//                        self.show_add_feed_view.toggle()
+//                    }) {
+//                        Label("Add Feeds", systemImage: "plus")
+//                    }
+//
+//                    Button(action: {
+//                        print("Remove Feed from Collection Button clicked.")
+//                    }) {
+//                        Label("Remove Feed", systemImage: "trash")
+//                    }
+//                }
+//                label: {
+//                    Label("Edit", systemImage: "square.and.pencil").imageScale(.large)
+//                }
+//            }
+//        }.sheet(isPresented: self.$show_add_feed_view) {
+//            AddFeedsToCollectionView(collection: collection)
+//        }
+       // */
         
         //lines 139-141 are part of the workaround for back button bug
 //        Button(action:{ self.presentationMode.wrappedValue.dismiss() }){
 //                    Text("Go Back")
 //                }
-   
+
+        
+        
     }
     
+    /**
+     * ToDo: Add comment
+     */
+    func checkBla(feed: NewsFeed) -> Bool {
+        for coll_feed in collection.feed_list {
+            if (coll_feed.id == feed.id) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    /**
+     * ToDo: Add comment
+     */
+    private func onAdd() {
+            print("add feed to coll btn pressed")
+        }
+    
+    /**
+     * ToDo: Add comment
+     */
     func delete(at offsets: IndexSet) {
         print("delete feed from coll btn pressed")
     }
