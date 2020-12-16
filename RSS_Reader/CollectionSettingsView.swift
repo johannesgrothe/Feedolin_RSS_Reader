76ddad1bde40d3019b2bc3a0e3e8e412615a2331
Katharina J. Kühn <kjohannakuehn@googlemail.com>
2020-12-16 18:20:32 +0100

    #43 add edit mode to collections, implement remove and add colls

--- RSS_Reader/CollectionSettingsView.swift
+++ RSS_Reader/CollectionSettingsView.swift
@@ -12,48 +12,73 @@ import SwiftUI
  * Here you see a list of all collections.
  */
 struct CollectionSettingsView: View {
-    var body: some View {
-        CollectionSettingsList()
-            .navigationBarTitle("Collection Settings")
-            .toolbar {
-                ToolbarItem(placement: .primaryAction) {
-                    Menu {
-                        Button(action: {
-                            print("Add New Collection Button clicked.")
-                        }) {
-                            Label("Add Collection", systemImage: "plus")
-                        }
-                        
-                        Button(action: {
-                            print("Remove Collection Button clicked.")
-                        }) {
-                            Label("Remove Collection", systemImage: "trash")
-                        }
-                    }
-                    label: {
-                        Label("Edit", systemImage: "square.and.pencil").imageScale(.large)
-                    }
-                }
-            }
-    }
-}
-
-/**
- * Displayes the List of all collections
- */
-struct CollectionSettingsList: View {
-
+    
     /**
      * The model
      */
     @ObservedObject var model: Model = .shared
     
+    /**
+     * indicates if the user is in edit_mode to add or remove collections
+     */
+    @State private var edit_mode = false
+    
+    /**
+     * ToDo: Add comment
+     */
+    @State var new_coll_name: String = "NewCollection"
+    
     var body: some View {
+
+//    CollectionSettingsList()
         List {
-            ForEach(model.collection_data) { collection in
-                NavigationLink(destination: CollectionDetailSettingsView(collection: collection)) {
-                    Text(collection.name).font(.headline)
+        
+            if (edit_mode) {
+                HStack {
+                    TextField("Add a new collection name...", text: $new_coll_name)
+                    
+                    Spacer()
+                    Button(action: {
+                        print("Add Collection Button clicked.")
+                        model.collection_data.append(Collection(name: new_coll_name))
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                            .foregroundColor(.green)
+                    }
+                    
                 }
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .listRowBackground(Color.clear)
+            }
+            
+            ForEach(model.collection_data) { collection in
+                
+                if (edit_mode) {
+                
+                    /**
+                     * ToDo: Add comment
+                     */
+                    HStack {
+                        Text(collection.name).font(.headline)
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            print("Delete Collection Button clicked.")
+                            // ToDo: call delete coll methode
+                            model.collection_data.removeAll( where: { $0.id == collection.id })
+                        }) {
+                            Image(systemName: "minus.circle.fill")
+                                .foregroundColor(.red)
+                        }
+                    }
+                    
+                } else {
+                    NavigationLink(destination: CollectionDetailSettingsView(collection: collection)) {
+                        Text(collection.name).font(.headline)
+                    }
+                }
+                    
             }
         .listRowBackground(Color.clear)
         }
@@ -63,5 +88,50 @@ struct CollectionSettingsList: View {
         })
         .background(Color(UIColor(named: "BackgroundColor")!))
         .edgesIgnoringSafeArea(.bottom)
+        .navigationTitle("Collection Settings")
+        .navigationBarItems(trailing:
+            Button(action: {
+                print("Edit collection btn clicked")
+                if (edit_mode) {
+                    edit_mode = false
+                } else {
+                    edit_mode = true
+                }
+            }) {
+                if (edit_mode) {
+                    Text("Done")
+                } else {
+                    Text("Edit")
+                }
+            })
     }
 }
+
+
+///**
+// * Displayes the List of all collections
+// */
+//struct CollectionSettingsList: View {
+//
+//    /**
+//     * The model
+//     */
+//    @ObservedObject var model: Model = .shared
+//    
+//    var body: some View {
+//        List {
+//            ForEach(model.collection_data) { collection in
+//                NavigationLink(destination: CollectionDetailSettingsView(collection: collection)) {
+//                    Text(collection.name).font(.headline)
+//                }
+//            }
+//        .listRowBackground(Color.clear)
+//        }
+//        .onAppear(perform: {
+//            UITableView.appearance().backgroundColor = .clear
+//            UITableViewCell.appearance().backgroundColor = .clear
+//        })
+//        .background(Color(UIColor(named: "BackgroundColor")!))
+//        .edgesIgnoringSafeArea(.bottom)
+//    }
+//}
