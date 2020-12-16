28af0e4fdf2704c49d2b2f31565ce38b8dd1ac21
Johannes Grothe <johannesgrothe@web.de>
2020-12-16 19:59:50 +0100

    #71 Cleaned up search bar, added casing selector

--- RSS_Reader/ArticleList.swift
+++ RSS_Reader/ArticleList.swift
@@ -15,36 +15,59 @@ struct ArticleList: View {
     @ObservedObject var model: Model = .shared
     
     @State private var search_phrase = ""
-    @State private var show_cancel_button = false
+    @State private var search_ignore_casing = true
     
     var body: some View {
         List {
             
             // Search view
-                    HStack {
-                       //search bar magnifying glass image
-                       Image(systemName: "magnifyingglass").foregroundColor(.secondary)
+            HStack {
+                HStack {
+                    //search bar magnifying glass image
+                    Image(systemName: "magnifyingglass").foregroundColor(.secondary)
                                 
-                       //search bar text field
-                       TextField("search", text: self.$search_phrase, onEditingChanged: { isEditing in
-                       self.show_cancel_button = true
-                       })
+                    //search bar text field
+                    TextField("search", text: $search_phrase)
                        
-                       // x Button
-                       Button(action: {
-                           self.search_phrase = ""
-                       }) {
-                           Image(systemName: "xmark.circle.fill")
-                                .foregroundColor(.secondary)
-                                  .opacity(self.search_phrase == "" ? 0 : 1)
-                          }
+                    // x Button
+                    Button(action: {
+                        search_phrase = ""
+                    }) {
+                        Image(systemName: "xmark.circle.fill")
+                            .foregroundColor(.secondary)
+                            .opacity(search_phrase == "" ? 0 : 1)
+                    }
+                    
+                    // Casing selector
+                    Button(action: {
+                        search_ignore_casing = !search_ignore_casing
+                        print("Ignore Casing set to \(search_ignore_casing)")
+                    }) {
+                        if search_ignore_casing {
+                            Image(systemName: "textformat.size.larger")
+                                 .foregroundColor(.secondary)
+                        } else {
+                            Image(systemName: "textformat")
+                                 .foregroundColor(.secondary)
+                        }
+                    }
+                    
                 }
-                 .padding(8)
-                 .background(Color(.secondarySystemBackground))
-                 .cornerRadius(8)
+                .padding(8)
+                .background(Color(.secondarySystemBackground))
+                .cornerRadius(8)
+                .listRowBackground(Color.clear)
             
+            }
+            .listRowBackground(Color.clear)
             
-            let search_obj = SearchPhrase(pattern: search_phrase, is_regex: false, search_description: true, ignore_casing: true)
+            /** Create search object */
+            let search_obj = SearchPhrase(pattern: search_phrase,
+                                          is_regex: false,
+                                          search_description: true,
+                                          ignore_casing: search_ignore_casing)
+            
+            /** filter article list */
             let display_list = model.filtered_article_data.filter { (article) -> Bool in
                 if !self.search_phrase.isEmpty {
                     return search_obj.matchesArticle(article)
@@ -52,11 +75,11 @@ struct ArticleList: View {
                 return true
             }
             
-            // for preview  model.stored_article_data and not model.filtered_article_data
             ForEach(display_list){ article in
                 ArticleListRow(article: article)
             }
             .listRowBackground(Color.clear)
+
         }
         .edgesIgnoringSafeArea(.bottom)
         .listStyle(PlainListStyle())
