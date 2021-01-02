35e1e3a60b35bbc55f22cbfc15b4038253201aef
Katharina J. Kühn <kjohannakuehn@googlemail.com>
2021-01-02 13:03:52 +0100

    #53 add infos for reviewer

--- RSS_Reader/FeedDataStructures.swift
+++ RSS_Reader/FeedDataStructures.swift
@@ -135,8 +135,11 @@ class NewsFeedProvider: Codable, Identifiable, ObservableObject{
         /**
          Google-API for getting an icon for the url
          */
-//        self.icon = AsyncImage("https://www.google.com/s2/favicons?sz=64&domain_url=\(url)", default_image: default_icon)
-        self.icon = AsyncImage("https:/=\(url)", default_image: default_icon)
+        self.icon = AsyncImage("https://www.google.com/s2/favicons?sz=64&domain_url=\(url)", default_image: default_icon)
+        
+        // For testing fb53 uncomment line 129 and comment line 126 out
+//        self.icon = AsyncImage("https:/=\(url)", default_image: default_icon)
+        
         /**
          the icon_loaded_indicator is 'chained' to the images 'objectWillChange'
          This way, the NewsFeedProvider is changing as soon as the image is changing, updating
