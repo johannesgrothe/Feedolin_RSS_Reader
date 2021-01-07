9ce9446e3372ff7b33890dab4ae261a4de65608c
Kenanja Nuding <s78077@beuth-hochschule.de>
2021-01-07 16:19:59 +0100

    #58 Changed hole layout / image / textfields
    Changed comments completely
    Changed 'kürzel' to 'abbreviation' (now its english)
    Edited Image and Change Image button to a nice layout
    Deleted Save button now save on disappear
    Changed Background without UIColor
    Changed textfield to new Layout with x-btn

--- RSS_Reader/FeedProviderSettingsView.swift
+++ RSS_Reader/FeedProviderSettingsView.swift
@@ -12,68 +12,132 @@ import SwiftUI
  */
 struct FeedProviderSettingsView: View {
     
+    /**
+     @feed_provider is the NewsFeedProvider to be editing
+     */
     let feed_provider: NewsFeedProvider
+    /**
+     @name is a string that holds the input of the user
+     */
     @State private var name = ""
-    @State private var short_name = ""
-    
+    /**
+     @abbreviation also token or short_name is a string that holds the input of the user
+     */
+    @State private var abbreviation = ""
+    /**
+     @isEditing is the indicator if the user is editing the textfield
+     */
+    @State private var isEditing: Bool = false
+    /**
+     @presentationMode make the View dismiss itself
+     */
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
-        VStack {
-            // Dummy image for now
-            feed_provider.icon.img
-                .resizable()
-                .frame(width: 130, height: 130)
-                .padding(.horizontal, 15.0)
-                .padding(.vertical, 15.0)
-            
-            Button(action: {
-                print("change image pressed")
-            }) {
-                Text("Change Image")
+        // hole content
+        VStack(spacing: 20)  {
+            // image and change-button
+            ZStack{
+                // image of provider
+                feed_provider.icon.img
+                    .resizable()
+                    .frame(width: 140, height: 140)
+                    .cornerRadius(100)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 100)
+                            .stroke(Color("ButtonColor"), lineWidth: 4)
+                            .foregroundColor(.clear)
+                    )
+                // background of change image button
+                Circle()
+                    .frame(width: 39, height: 39)
+                    .foregroundColor(Color("BackgroundColor"))
+                    .offset(x: 50, y: 50)
+                
+                // change image button
+                Button(action: {
+                    print("change image pressed")
+                }) {
+                    Image(systemName: "pencil.circle")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                        
+                }
+                .foregroundColor(Color("ButtonColor"))
+                .offset(x: 50, y: 50)
+                
             }
+            .frame(width: 140, height: 140, alignment: .leading)
+            .padding(.horizontal)
             
+            // row with name and textfield
             HStack {
                 Text("Name")
-                    .padding(.horizontal, 25.0)
-                TextField(
-                    feed_provider.name,
-                    text: $name
-                ).textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.horizontal, 25.0)
+                    .padding(.trailing)
+                // input textfield
+                HStack{
+                    TextField(feed_provider.name, text: $name) { isEditing in
+                        self.isEditing = isEditing
+                        } onCommit: {
+                            feed_provider.name = name
+                        }
+                    // x-button
+                    Button(action: {
+                        name = ""
+                    }) {
+                        Image(systemName: "xmark.circle.fill")
+                            .opacity(name == "" ? 0 : 1)
+                    }
+                    .buttonStyle(BorderlessButtonStyle())
+                }
+                .padding(10)
+                .background(Color(.secondarySystemBackground))
+                .foregroundColor(.secondary)
+                .cornerRadius(10)
             }
+            .padding(.horizontal)
             
+            // row with abbreviation and textfield
             HStack {
-                Text("Kürzel")
-                    .padding(.horizontal, 25.0)
-                Spacer()
-                TextField(
-                    feed_provider.token,
-                    text: $short_name
-                ).textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.horizontal, 25.0)
+                Text("Abbreviation")
+                    .padding(.trailing)
+                // input textfield
+                HStack{
+                    TextField(feed_provider.token, text: $abbreviation) { isEditing in
+                        self.isEditing = isEditing
+                        } onCommit: {
+                            feed_provider.token = abbreviation
+                        }
+                    // x-button
+                    Button(action: {
+                        abbreviation = ""
+                    }) {
+                        Image(systemName: "xmark.circle.fill")
+                            .opacity(abbreviation == "" ? 0 : 1)
+                    }
+                    .buttonStyle(BorderlessButtonStyle())
+                }
+                .padding(10)
+                .background(Color(.secondarySystemBackground))
+                .foregroundColor(.secondary)
+                .cornerRadius(10)
             }
+            .padding(.horizontal)
             
             Spacer()
         }
-        .navigationBarTitle(feed_provider.url, displayMode: .inline)
-        .navigationBarItems(
-            trailing:
-                Button(action: {
-                    print("save_btn pressed")
-                    if name != "" {
-                        feed_provider.name = name
-                    }
-                    if short_name != "" {
-                        feed_provider.token = short_name
-                    }
-                    self.presentationMode.wrappedValue.dismiss()
-                }) {
-                    Text("Save")
-                }
-        )
-        .background(Color(UIColor(named: "BackgroundColor")!))
+        .padding(10)
+        .background(Color("BackgroundColor"))
         .edgesIgnoringSafeArea(.bottom)
+        .navigationBarTitle(feed_provider.url, displayMode: .inline)
+        .onDisappear(perform: {
+            if name != "" {
+                feed_provider.name = name
+            }
+            if abbreviation != "" {
+                feed_provider.token = abbreviation
+            }
+        })
     }
 }
 
