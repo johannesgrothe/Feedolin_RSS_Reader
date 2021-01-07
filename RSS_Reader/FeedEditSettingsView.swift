74e9b74b603faf6ee8a872ce3356dd010b36dd51
Kenanja Nuding <s78077@beuth-hochschule.de>
2021-01-07 14:01:36 +0100

    #58 Changed layout and more
    Added x-button in textfield
    Changed/Added Comments
    Changed layout with paddings an styles
    Changed the way the name is saved (deleted save btn)

--- RSS_Reader/FeedEditSettingsView.swift
+++ RSS_Reader/FeedEditSettingsView.swift
@@ -12,8 +12,10 @@ import SwiftUI
  */
 struct FeedEditSettingsView: View {
     
+    /**
+     @feed is the NewsFeed to be editing
+     */
     let feed: NewsFeed
-    
     /**
      @show_in_main is a boolean that shows if a feed is visible in the FeedList or not
      */
@@ -26,6 +28,10 @@ struct FeedEditSettingsView: View {
      @name is a string that holds the input of the user
      */
     @State private var name = ""
+    /**
+     @isEditing is the indicator if the user is editing the textfield
+     */
+    @State private var isEditing: Bool = false
     
     /**
      @showingAlert shows if alert is true*/
@@ -35,22 +41,41 @@ struct FeedEditSettingsView: View {
      @presentationMode make the View dismiss itself
      */
     @Environment(\.presentationMode) var presentationMode
-    
+    /**
+     @model is the shared model singelton
+     */
     @ObservedObject var model: Model = .shared
     
     var body: some View {
-        VStack {
-            /** TextField where the users gives a new name for the feed*/
+        // hole Content
+        VStack(alignment: .leading, spacing: 20) {
+            // Row with Name and Textfield
             HStack {
                 Text("Name")
-                    .padding(20.0)
-                TextField(
-                    feed.name,
-                    text: $name
-                ).textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.horizontal, 20.0)
+                    .padding(.trailing)
+                // Input Textfield
+                HStack {
+                    TextField(feed.name, text: $name) { isEditing in
+                        self.isEditing = isEditing
+                        } onCommit: {
+                            feed.name = name
+                        }
+                    // x Button
+                    Button(action: {
+                        name = ""
+                    }) {
+                        Image(systemName: "xmark.circle.fill")
+                            .opacity(name == "" ? 0 : 1)
+                    }
+                    .buttonStyle(BorderlessButtonStyle())
+                }
+                .padding(10)
+                .background(Color(.secondarySystemBackground))
+                .foregroundColor(.secondary)
+                .cornerRadius(10)
             }
-            /**Toggle Button where user can set if the current feed will be shown on mainview*/
+            .padding(.horizontal)
+            
             HStack{
                 Toggle("Show in Main Feed", isOn: $show_in_main)
                     .onAppear(){
@@ -63,29 +88,29 @@ struct FeedEditSettingsView: View {
                     .onDisappear(){
                         model.refreshFilter()
                     }
-                    .padding(.horizontal,20.0)
             }
-            /**Toggle Button where user can set if the current feed will be use filters*/
-
+            .padding(.horizontal)
+            
             HStack{
                 Toggle("Use Filters",isOn: $use_filters)
                     .onAppear(){
-                        show_in_main = feed.show_in_main
+                        use_filters = feed.use_filters
                     }
                     .onChange(of: use_filters){value in
                         feed.use_filters = value
                         print("Called toggle User Filters = \(feed.use_filters)")
                     }
-                    
-                    .padding(.horizontal,20.0)
-                
             }
+            .padding(.horizontal)
             
-            List{
-                NavigationLink(destination: DummyDetailView()){
-                    Text("Filters Selected")
-                }
+            NavigationLink(destination: DummyDetailView()){
+                Text("Filters Selected")
+                Spacer()
+                Image(systemName: "chevron.forward")
             }
+            .padding(.horizontal)
+            
+            Spacer()
             
             Button(action: {
                 withAnimation {
@@ -107,18 +132,12 @@ struct FeedEditSettingsView: View {
             
             Spacer()
         }
+        .padding(10)
         .navigationBarTitle(feed.name, displayMode: .inline)
-        .navigationBarItems(
-            trailing:
-                Button(action: {
-                    print("save_btn pressed")
-                    if name != "" {
-                        feed.name = name
-                    }
-                    self.presentationMode.wrappedValue.dismiss()
-                }) {
-                    Text("Save")
-                }
-        )
+        .onDisappear(perform: {
+            if name != "" {
+                feed.name = name
+            }
+        })
     }
 }
