c735352888f317fd49c358b812a4c993d314ab52
Johannes Grothe <johannesgrothe@web.de>
2021-01-16 22:37:37 +0100

    #115 Added UITests Scheme and added tests to gitlab-ci.yml

--- RSS_ReaderTests/RSS_ReaderTests.swift
+++ RSS_ReaderTests/RSS_ReaderTests.swift
@@ -17,21 +17,38 @@ class RSS_ReaderTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
     
     func testStringExtensions() {
+        testStrEx_chopPrefix()
+        testStrEx_isAlphanumeric()
+    }
+    
+    func testStrEx_chopPrefix() {
         XCTAssertTrue("test yolokopter".chopPrefix(5) == "yolokopter")
     }
+    
+    func testStrEx_isAlphanumeric() {
+        XCTAssertTrue("1211".isAlphanumeric)
+        XCTAssertTrue("1211ad".isAlphanumeric)
+        XCTAssertTrue("dd12er11tz".isAlphanumeric)
+        XCTAssertTrue(!"".isAlphanumeric)
+        XCTAssertTrue(!"add$".isAlphanumeric)
+        XCTAssertTrue(!"ad%d".isAlphanumeric)
+    }
 
+    /**
+     func testExample() throws {
+         // This is an example of a functional test case.
+         // Use XCTAssert and related functions to verify your tests produce the correct results.
+     }
+     */
+    
+    /**
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    */
 }
